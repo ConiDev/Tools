fa69068e63fb47050efdeac64f691cdfc29b6490
nduvelle <nduvelle@gmail.com>
2021-06-20 21:21:01 +0200

    update for Bonus

--- Born2beroot/monitoring.sh
+++ Born2beroot/monitoring.sh
@@ -8,24 +8,34 @@ DISKU=$(df -h | awk '$NF=="/"{printf "%d/%dGB (%s)\n", $3,$2,$5}')
 CPUU=$(mpstat | grep "all" | awk '{print (100 - $NF)}')
 LBOOT=$(who -a | grep "syst" | awk '{print $3" "$4}')
 LVM=$(/root/Tools/Born2beroot/Check_Script/LVM_check/LVM_inuse.sh)
-TCP=$(ss | grep "tcp" | wc -l)
+TCP=$(ss -tH | grep "ESTAB" | wc -l)
 USER=$(who | wc -l)
 IP=$(ip a | grep "inet " | tail -1 | awk '{print $2}' | cut -d / -f 1)
 MAC=$(ip a | grep "ether " | awk '{print $2}')
 SUDO=$(cat /var/log/sudo/sudo.log | grep "COMMAND" | wc -l)
 
-MONITOR=$(echo "#Architecture: $ARCHY" > /root/Tools/Born2beroot/monitor.txt; \
-      	echo "#CPU physical : $PPROC" >> /root/Tools/Born2beroot/monitor.txt; \
-	echo "#vCPU : $VCPU" >> /root/Tools/Born2beroot/monitor.txt; \
-	echo "#Memory Usage: $MEMU" >> /root/Tools/Born2beroot/monitor.txt; \
-	echo "#Disk Usage: $DISKU" >> /root/Tools/Born2beroot/monitor.txt; \
-	echo "#CPU load: $CPUU %" >> /root/Tools/Born2beroot/monitor.txt; \
-	echo "#Last boot: $LBOOT" >> /root/Tools/Born2beroot/monitor.txt; \
-	echo "#LVM use: $LVM" >> /root/Tools/Born2beroot/monitor.txt; \
-	echo "#Connexions TCP : $TCP ESTABLISHED" >> /root/Tools/Born2beroot/monitor.txt; \
-	echo "#User log: $USER" >> /root/Tools/Born2beroot/monitor.txt; \
-	echo "#Network: IP $IP ($MAC)" >> /root/Tools/Born2beroot/monitor.txt; \
-	echo "#Sudo : $SUDO cmd" >> /root/Tools/Born2beroot/monitor.txt; \
-	)
+echo -e "\n\t#Architecture: $ARCHY
+\t#CPU physical : $PPROC
+\t#vCPU : $VCPU
+\t#Memory Usage: $MEMU
+\t#Disk Usage: $DISKU
+\t#CPU load: $CPUU %
+\t#Last boot: $LBOOT
+\t#LVM use: $LVM
+\t#Connexions TCP : $TCP ESTABLISHED
+\t#User log: $USER
+\t#Network: IP $IP ($MAC)
+\t#Sudo : $SUDO cmd" | wall
 
-wall /root/Tools/Born2beroot/monitor.txt
+echo -e "\n\t#Architecture: $ARCHY
+\t#CPU physical : $PPROC
+\t#vCPU : $VCPU
+\t#Memory Usage: $MEMU
+\t#Disk Usage: $DISKU
+\t#CPU load: $CPUU %
+\t#Last boot: $LBOOT
+\t#LVM use: $LVM
+\t#Connexions TCP : $TCP ESTABLISHED
+\t#User log: $USER
+\t#Network: IP $IP ($MAC)
+\t#Sudo : $SUDO cmd" > /root/Born2beroot/monitor.txt
